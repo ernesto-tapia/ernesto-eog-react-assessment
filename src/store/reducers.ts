6d2115d942c781df65813f8cc886c04345b9ed50
ernesto-tapia <ernesto.tapia94@hotmail.com>
2021-02-13 18:58:24 -0700

    Add selectedMetrics to reducer

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,7 +1,8 @@
 import { reducer as weatherReducer } from '../Features/Weather/reducer';
 import { reducer as measurementsReducer } from '../Features/Measurements/reducer';
-
+import { reducer as selectedMetricsReducer } from '../Features/SelectedMetrics/reducer';
 export default {
   weather: weatherReducer,
   measurements: measurementsReducer,
+  selectedMetrics: selectedMetricsReducer,
 };
